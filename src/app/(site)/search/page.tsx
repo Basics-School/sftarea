365eabf38b3286e5d6bc681ec77d0d0fa1702c57
Kazi Tanvirul Islam Sakib <ktisakib@gmail.com>
2024-12-10 18:38:09 +0000

    Wrap search page providers in Suspense for improved loading handling

--- src/app/(site)/search/page.tsx
+++ src/app/(site)/search/page.tsx
@@ -1,16 +1,18 @@
 import { FilterProvider } from '@/components/contexts/filter-context'
 import { MapProvider } from '@/components/contexts/map-context'
 import PropertySearch from '@/components/property-search'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const SearchPage = () => {
     return (
         <main>
-            <FilterProvider>
-                <MapProvider>
-                    <PropertySearch />
-                </MapProvider>
-            </FilterProvider>
+            <Suspense fallback={null}>
+                <FilterProvider>
+                    <MapProvider>
+                        <PropertySearch />
+                    </MapProvider>
+                </FilterProvider>
+            </Suspense>
         </main>
     )
 }
